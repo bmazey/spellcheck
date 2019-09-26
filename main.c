02b57deefdb5e10cd5e81817ea4de96dd4ec620f
Brandon <b.mazey@nyu.edu>
2019-09-26 14:44:06 -0400

    seems to be working!

--- main.c
+++ main.c
@@ -7,7 +7,20 @@
 
 hashmap_t hashmap[HASH_SIZE];
 
+// helper function to print loaded dictionary hashtable list values
+void printList(struct node* n)
+{
+    while (n != NULL) {
+        printf(" %s \n", n->word);
+        n = n->next;
+    }
+}
+
 int main() {
     bool loaded = load_dictionary("C:\\Users\\Brandon\\CLionProjects\\spellcheck\\wordlist.txt", hashmap);
     printf(loaded ? "true" : "false");
+
+    for(int i = 0; i < HASH_SIZE; i++) {
+        printList(hashmap[i]);
+    }
 }
